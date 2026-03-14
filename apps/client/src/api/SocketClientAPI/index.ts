d14f86e347b02a70b9da2703815d5ba265ec2b74
mvavilin <realvavilin@yandex.ru>
2026-03-15 00:31:53 +0300

    feat: add Toast component with 4 variants

--- apps/client/src/api/SocketClientAPI/index.ts
+++ apps/client/src/api/SocketClientAPI/index.ts
@@ -1,3 +1,3 @@
-export { default as BaseSocketClient } from '@/api/SocketClientAPI/BaseSocketClient';
+export { default as BaseSocketClient } from '@api/SocketClientAPI/BaseSocketClient';
 
-export { default as socketClient } from '@/api/SocketClientAPI/GameSocketClient';
+export { default as socketClient } from '@api/SocketClientAPI/SocketClient';
