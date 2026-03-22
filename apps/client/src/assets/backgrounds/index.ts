fb8c1a446a98b97924e230d43582c92072402a6b
mvavilin <realvavilin@yandex.ru>
2026-03-22 18:40:45 +0300

    feat: add 404 page

--- apps/client/src/assets/backgrounds/index.ts
+++ apps/client/src/assets/backgrounds/index.ts
@@ -1,1 +1,3 @@
 export { default as LOBBY_PAGE_BACKGROUND } from '@assets/backgrounds/lobby-page-background.jpg';
+
+export { default as NOT_FOUND_PAGE_BACKGROUND } from '@assets/backgrounds/not-found-page-background.jpeg';
