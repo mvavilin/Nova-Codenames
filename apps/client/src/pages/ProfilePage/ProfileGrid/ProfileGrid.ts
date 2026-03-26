083a0c43f3cfd728459e82157c1313a8a297750c
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-03-26 16:34:50 +0300

    feat: add profile grid

--- apps/client/src/pages/ProfilePage/ProfileGrid/ProfileGrid.ts
+++ apps/client/src/pages/ProfilePage/ProfileGrid/ProfileGrid.ts
@@ -1,30 +1,33 @@
-import BaseComponent from '@/api/ComponentsAPI/base/BaseComponent';
+import type { ProfileGridProperties } from './ProfileGrid.types';
+
+import { ContainerComponent } from '@ComponentsAPI';
 import StatsCard from '../cards/StatsCard';
 import RolesCard from '../cards/RolesCard';
 import MatchHistoryCard from '../cards/MatchHistoryCard';
 import AchievementsCard from '../cards/AchievementsCard';
 
-export default class ProfileGrid extends BaseComponent {
-  constructor() {
+export default class ProfileGrid extends ContainerComponent {
+  constructor({ ...rest }: ProfileGridProperties = {}) {
     super({
-      tag: 'div',
+      tag: 'main',
       classes: 'grid grid-cols-2 gap-6',
+      ...rest,
     });
 
     this.setChildren([this.createLeft(), this.createRight()]);
   }
 
-  private createLeft(): BaseComponent {
-    return new BaseComponent({
-      tag: 'div',
+  private createLeft(): ContainerComponent {
+    return new ContainerComponent({
+      tag: 'section',
       classes: 'flex flex-col gap-6',
       children: [new StatsCard(), new RolesCard()],
     });
   }
 
-  private createRight(): BaseComponent {
-    return new BaseComponent({
-      tag: 'div',
+  private createRight(): ContainerComponent {
+    return new ContainerComponent({
+      tag: 'section',
       classes: 'flex flex-col gap-6',
       children: [new MatchHistoryCard(), new AchievementsCard()],
     });
