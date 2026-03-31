526167ed9830ba8e567b6b744c8530ea0729b369
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-03-31 14:11:59 +0300

    refactor: remove AboutUs and GameRules to components

---
+++ apps/client/src/components/BaseModal/BaseModal.ts
@@ -0,0 +1,40 @@
+// BaseModal.ts
+import type { BaseComponent } from '@/api/ComponentsAPI';
+import { Modal, Button } from '@/components/ui';
+import { t } from '@/i18n';
+import { TranslationKeys } from '@/i18n/translationKeys';
+
+export default class BaseModal extends Modal {
+  constructor(children: BaseComponent[]) {
+    super({
+      children,
+      isClosable: true,
+    });
+
+    this.setClasses(
+      '!px-0 !py-0 rounded-lg !bg-[var(--color-dark)] border border-[var(--color-brand)]'
+    );
+
+    this.setupCloseIcon();
+    this.setupCloseButton();
+  }
+
+  private setupCloseIcon(): void {
+    const close = this.children[1];
+    if (close) {
+      close.destroyChildren();
+      close.setContent('📌');
+    }
+  }
+
+  private setupCloseButton(): void {
+    this.appendChildren(
+      new Button({
+        label: t(TranslationKeys.GAME_RULES_CLOSE_BTN),
+        classes:
+          'mb-6 mx-auto !bg-[var(--color-brand)] hover:!bg-[var(--color-accent)] !font-brand !text-[var(--color-dark)] transition-colors duration-200',
+        onClick: (): Modal => this.hide(),
+      })
+    );
+  }
+}
