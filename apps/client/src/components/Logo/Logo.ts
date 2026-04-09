18af28134ce0999a45f8c8529c2d290d290955fc
mvavilin <realvavilin@yandex.ru>
2026-04-09 14:28:46 +0300

    refactor: change logo navigation on game and room pages

--- apps/client/src/components/Logo/Logo.ts
+++ apps/client/src/components/Logo/Logo.ts
@@ -1,14 +1,20 @@
 import { router } from '@router';
 import { TextComponent } from '@ComponentsAPI';
+import { URLS } from '@RouterAPI/router.constants';
 
-const LOGO_CLASSES = `font-brand font-normal text-2xl leading-none tracking-[0.01rem] text-transparent [-webkit-text-stroke:1px_var(--color-brand)] text-center sm:text-left cursor-pointer`;
+const LOGO_CLASSES = `font-brand font-normal text-2xl leading-none tracking-[0.01rem] text-transparent [-webkit-text-stroke:1px_var(--color-brand)] text-center sm:text-left`;
 
 export default class Logo extends TextComponent {
   constructor() {
     super({
-      classes: LOGO_CLASSES,
+      classes:
+        `${LOGO_CLASSES} ${globalThis.location.pathname === URLS.LOBBY() ? 'cursor-pointer' : ''}`.trim(),
       content: 'Nova Codenames Game',
-      listeners: { click: () => router.navigate() },
+      listeners: {
+        click: () => {
+          if (globalThis.location.pathname === URLS.LOBBY()) router.navigate();
+        },
+      },
     });
   }
 }
